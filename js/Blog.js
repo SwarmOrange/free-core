6741374ecdd9ecbcf8b6ae710d4c97470ea2cca5
shadurin <igor.shadurin@gmail.com>
2018-10-09 17:34:59 +0300

    added timestamp

--- js/Blog.js
+++ js/Blog.js
@@ -457,8 +457,9 @@ class Blog {
         return this.swarm.post(null, data, null, null, 'bzz-resource:');
     }
 
-    saveMessage(receiverHash, message, afterReceiverMessage, afterMessageId, isPrivate) {
+    saveMessage(receiverHash, message, afterReceiverMessage, afterMessageId, timestamp, isPrivate) {
         let self = this;
+        timestamp = timestamp || +new Date();
         if (isPrivate) {
             throw('Private messages not supported');
         }
@@ -484,6 +485,7 @@ class Blog {
 
             let data = {
                 id: messageId,
+                timestamp: timestamp,
                 after_receiver_message: afterReceiverMessage,
                 after_message_id: afterMessageId,
                 receiver_hash: receiverHash,
